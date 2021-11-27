19cf9a52904bc85f84b2c5cad3f10874a0a89cd7
SIOUkoeran <alstnalsgud@gmail.com>
2021-11-27 19:32:31 +0900

    Fix ListType 초기화 실패

---
+++ src/test/java/com/innohotsource/hotjson/Json/JsonListToObjectTest.java
@@ -0,0 +1,47 @@
+package com.innohotsource.hotjson.Json;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.Test;
+
+public class JsonListToObjectTest {
+    
+    @Test
+    void ListToObject(){
+        JSONObject sample = new JSONObject();
+        sample.put("name", "sampleName");
+        sample.put("id", 1L);
+
+        JSONObject subSample = new JSONObject();
+        subSample.put("subName", "sub1");
+        subSample.put("subId", 1L);
+
+        JSONObject subSample1 = new JSONObject();
+        subSample.put("subName", "sub2");
+        subSample.put("subId", 2L);
+
+        JSONObject Sub = new JSONObject();
+        Sub.put("subSubName", "subsub");
+        Sub.put("subSubId", 1L);
+
+        JSONObject Sub1 = new JSONObject();
+        Sub1.put("subSubName", "subsub");
+        Sub1.put("subSubId", 2L);
+
+        JSONArray subList = new JSONArray();
+        subList.add(Sub);
+        subList.add(Sub1);
+
+        subSample.put("subList",subList);
+
+        JSONArray subSampleList = new JSONArray();
+        subSampleList.add(subSample);
+
+        sample.put("subSampleList", subSampleList);
+        System.out.println("sample = " + sample.toJSONString());
+
+        JsonToObject json = new JsonToObject();
+        Sample1 sample1 = (Sample1) json.fromJson(sample, new Sample1());
+        System.out.println("sample1.tostring = " + sample1.toString());
+    }
+}
--- src/test/java/com/innohotsource/hotjson/Json/Sub.java
+++ src/test/java/com/innohotsource/hotjson/Json/Sub.java
@@ -2,11 +2,7 @@ package com.innohotsource.hotjson.Json;
 
 public class Sub {
     private String subSubName;
+    private Long subSubId;
 
-    @Override
-    public String toString() {
-        return "Sub{" +
-                "subSubName='" + subSubName + '\'' +
-                '}';
-    }
+
 }
