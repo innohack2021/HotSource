b5de9cc394242596d283576a2439ab681b4d9403
SIOUkoeran <alstnalsgud@gmail.com>
2021-11-28 14:21:27 +0900

    Refactor 전체적인 코드취약점 #37

---
+++ src/test/java/com/innohotsource/hotjson/Json/sample/Sub.java
@@ -0,0 +1,14 @@
+package com.innohotsource.hotjson.Json.sample;
+
+public class Sub {
+    private String subSubName;
+    private Long subSubId;
+
+    @Override
+    public String toString() {
+        return "Sub{" +
+                "subSubName='" + subSubName + '\'' +
+                ", subSubId=" + subSubId +
+                '}';
+    }
+}
